1dbcf3f9287a35aa50a0c4ea4cb13e0744c60484
JC Martin <jcmartin@medallia.com>
2016-03-21 14:22:17 -0700

    Add timeout and cross state parameters

--- efsm.go
+++ efsm.go
@@ -7,19 +7,19 @@ import (
 	"log"
 )
 
-type Transition struct {
-	state  State
-	timeout *Timeout
-	events map[Event]func(State, Event) State
-}
 
 type Event string
 
-type State string
+type State struct {
+	Name        string
+	timeout     *Timeout
+	transitions map[Event]func(f *FSM, s *State, e Event) *State
+	Value       interface{}
+}
 
 type Timeout struct {
-	Name string
-	duration time.Duration
+	EventName string
+	duration  time.Duration
 }
 
 // state x event => action => [state',...]
@@ -28,71 +28,102 @@ type Timeout struct {
 
 type FSM struct {
 	sync.Mutex
-	CurrentState State
-	States       map[State]State
+	CurrentState string
+	States       map[string]*State
 	Events       chan Event
-	transitions  map[State]*Transition
 }
 
-func (f *FSM) setCurrentState(s State) error {
-	if _, ok := f.States[s]; ok {
-		f.CurrentState = s
-		if t, ok := f.transitions[s]; ok {
-			if t.timeout != nil {
-				timer := time.NewTimer(t.timeout.duration)
-				go func() {
-					<- timer.C
-					log.Printf("Timeout: %s",t.timeout.Name)
-					f.Events <- Event(t.timeout.Name)
-				}()
-			}
+func (f *FSM) setCurrentState(newState *State) error {
+	if s, ok := f.States[newState.Name]; ok {
+		f.CurrentState = newState.Name
+		if newState.timeout != nil {
+			s.timeout = newState.timeout
 		}
+		s.Value = newState.Value
+		if s.timeout != nil {
+			timer := time.NewTimer(s.timeout.duration)
+			go func() {
+				<-timer.C
+				log.Printf("Timeout: %s", s.timeout.EventName)
+				f.Events <- Event(s.timeout.EventName)
+			}()
+		}
+
 	} else {
 		return fmt.Errorf("Invalid State")
 	}
 	return nil
 }
 
-func (f *FSM) When(s State) *Transition {
-	t := Transition{
-		state: s,
-		timeout: nil,
-		events: make(map[Event]func(State, Event) State),
+func (f *FSM) Goto(name string) *State {
+	if s, ok := f.States[name]; ok {
+		f.CurrentState = name
+		return s
+	} else {
+		panic(fmt.Errorf("Unknown state to transition to: %s", name))
 	}
-	f.transitions[s] = &t
-	return &t
 }
 
-func (t *Transition) Timeout(timer *Timeout) *Transition {
-	t.timeout = timer
-	return t
+func (f *FSM) When(name string) *State {
+	if _, ok := f.States[name]; ok {
+		panic(fmt.Errorf("Duplicate state registration %s", name))
+	}
+	newState := &State{
+		Name:name,
+		timeout: nil,
+		transitions:  make(map[Event]func(f *FSM, s *State, e Event) *State),
+	}
+	f.States[name] = newState
+
+	return newState
 }
 
-func (t *Transition) Case(e Event, fn func(s State, e Event) State) error {
-	t.events[e] = fn
+func (s *State) With(v interface{}) *State {
+	s.Value = v
+	return s
+}
+
+func (s *State) ForMax(d time.Duration) *State {
+	s.timeout = &Timeout{
+		EventName: s.Name + "-timeout",
+		duration: d,
+	}
+	return s
+}
+
+func (s *State) Case(events []Event, fn func(f *FSM, s *State, e Event) *State ) error {
+	for _, e := range events {
+		s.transitions[e] = fn
+	}
 	return nil
 }
-func (f *FSM) Engine(initial State, c chan Event) error {
-	f.setCurrentState(initial)
+func (f *FSM) Engine(c chan Event) error {
 	f.Events = c
-	f.transitions = make(map[State]*Transition)
+	f.States = make(map[string]*State)
 	return nil
 }
 
-func (f *FSM) Run() error {
+func (f *FSM) Run(initial string) error {
 	log.Printf("In Run")
+	if s, ok := f.States[initial]; ok {
+		f.setCurrentState(s)
+	} else {
+		panic(fmt.Errorf("Unknown initial state: %s", initial))
+	}
+
 	for e := range f.Events {
-		if transitions, ok := f.transitions[f.CurrentState]; ok {
-			if fn, ok := transitions.events[e]; ok {
-				newState := fn(f.CurrentState, e)
-				if newState != "" {
-					log.Printf("--> %s\n", newState)
-					if err := f.setCurrentState(newState); err != nil {
-						panic(err)
-					}
+		f.Lock()
+		s := f.States[f.CurrentState]
+		if fn, ok := s.transitions[e]; ok {
+			newState := fn(f, s, e)
+			if newState != nil {
+				log.Printf("--> %s\n", newState.Name)
+				if err := f.setCurrentState(newState); err != nil {
+					panic(err)
 				}
 			}
 		}
+		f.Unlock()
 		// TODO process uncaught events
 	}
 	return nil
--- efsm_test.go
+++ efsm_test.go
@@ -9,39 +9,37 @@ func ExampleFSM() {
 
 	f := &FSM{}
 
-	f.States = map[State]State{
-		"Running":State("Running"),
-		"Rebooting":State("Rebooting"),
-		"Failed":State("Failed"),
-	}
-
 	c := make(chan Event, 1)
 
 	// initial state is Running
-	f.Engine("Running", c)
+	f.Engine(c)
 
 	var err error
 
-	// In state Running, when event reboot is received, go to Rebooting
-	err = f.When("Running").Case("reboot", func(s State, e Event)State {
-		fmt.Printf("Case %s %s\n", s, e)
-		return "Rebooting"
+	// In state Running, when event reboot or reset is received, go to Rebooting with a 5s timeout
+	err = f.When("Running").Case([]Event{"reboot", "reset"}, func(f *FSM, s *State, e Event) *State {
+		fmt.Printf("Case %s %s\n", s.Name, e)
+		x := 0
+
+		if s.Value != nil {
+
+			x = *s.Value.(*int)
+		}
+		fmt.Printf("Rebooting %d time\n", x)
+		x += 1
+		return f.Goto("Rebooting").ForMax(5 * time.Second).With(&x)
 	})
 	if err  != nil {
 		return
 	}
-	//Creating a timer to avoid staying in rebooting for ever
-	timer := Timeout{
-		Name: "boot_timeout",
-		duration: time.Duration(5 * time.Second),
-	}
-	// In state Rebooting, add a timeout of 10s
-	transition := f.When("Rebooting").Timeout(&timer)
+
+	s := f.When("Rebooting")
 
 	// If event booted is received go back to Running
-	err = transition.Case("booted", func(s State, e Event)State {
-		fmt.Printf("Case %s %s\n", s, e)
-		return "Running"
+	err = s.Case([]Event{"booted"}, func(f *FSM, s *State, e Event) *State {
+		fmt.Printf("Case %s %s\n", s.Name, e)
+
+		return f.Goto("Running").With(s.Value)
 	})
 
 	if err  != nil {
@@ -49,26 +47,35 @@ func ExampleFSM() {
 	}
 
 	// if timeout is received - moved to failed.
-	err = transition.Case("boot_timeout", func(s State, e Event)State {
-		fmt.Printf("Case %s %s\n", s, e)
-		return "Failed"
+	err = s.Case([]Event{"Rebooting-timeout"}, func(f *FSM, s *State, e Event) *State {
+		fmt.Printf("Case %s %s\n", s.Name, e)
+
+		return f.Goto("Failed").With(s.Value)
 	})
 
 	if err  != nil {
 		return
 	}
 
-	// In state Failed, when event reset is received, go to Rebooting
-	err = f.When("Failed").Case("reset", func(s State, e Event)State {
-		fmt.Printf("Case %s %s\n", s, e)
-		return "Rebooting"
+	// In state Failed, when event reset is received, go to Rebooting with a 5s timeout
+	err = f.When("Failed").Case([]Event{"reset"}, func(f *FSM, s *State, e Event) *State {
+		fmt.Printf("Case %s %s\n", s.Name, e)
+
+		x := 0
+		if s.Value != nil {
+			x = *s.Value.(*int)
+		}
+		fmt.Printf("Rebooting %d time\n", x)
+		x += 1
+
+		return f.Goto("Rebooting").ForMax(5 * time.Second).With(&x)
 	})
 	if err  != nil {
 		return
 	}
 
 	// Run the FSM engine
-	go f.Run()
+	go f.Run("Running")
 
 	// Send a reboot event and after 10s send a reset
 	go func() {
@@ -82,8 +89,10 @@ func ExampleFSM() {
 
 	//Output:
 	//Case Running reboot
-	//Case Rebooting boot_timeout
+	//Rebooting 0 time
+	//Case Rebooting Rebooting-timeout
 	//Case Failed reset
-	//Case Rebooting boot_timeout
+	//Rebooting 1 time
+	//Case Rebooting Rebooting-timeout
 	//Done
 }
